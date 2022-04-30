cff5a4708b0917d28d8aecc54897da26761534bb
SleepyHeadvvv <mohamedtouham55@gmail.com>
2022-04-30 12:04:30 +0200

    Added a "Live currency conversion mode" to convert different normal currency's and Bitcoin converter in real time.

--- main.py
+++ main.py
@@ -1,12 +1,15 @@
+from forex_python.converter import CurrencyRates
+from forex_python.bitcoin import BtcConverter
+force_decimal = True
+
 pi = 3.141592653589793238462643383279502884
-mode = input("pick a mode:\n(1)calculator mode\n(2)conversion mode\n-")
 
 
 # angle plan conversions units
 def angle_plan():
     while True:
         convertion = input("(1)radian to degree|(2)radian to grade\n(3)degree to radian|(4)degree to grade"
-                           "\n(5)grade to radian|(6)grade to degree\n-")
+                           "\n(5)grade to radian|(6)grade to degree\n- ")
         number = float(input("number to convert: "))
         if convertion == "1":
             print(number, "radian = ", number * 180 / pi, "degrees")
@@ -24,13 +27,13 @@ def angle_plan():
             print("Invalid option.")
             continue
         break
-        
+
 
 # time conversions units
 def time():
     while True:
         convertion = input("(1)Day to hour|(2)Day to minute\n(3)Hour to day|(4)Hour to minute\n"
-                           "(5)Minute to Day|(6)Minute to Hour")
+                           "(5)Minute to Day|(6)Minute to Hour\n- ")
         number = float(input("number to convert: "))
         if convertion == "1":
             print(number, "Day = ", number * 24, "Hour")
@@ -50,42 +53,84 @@ def time():
         break
 
 
-# calculator mode
-if mode == "1":
-    first_nb = float(input("first number"))
-    second_nb = float(input("second number"))
-
-    # if the user inputs something other than *, -, /, +
-    # the user will be asked to pick the operator again
+# currency conversion
+def currency_conversion():
     while True:
-        operator = input("pick an operator: *, -, /, +")
-        if operator == "*":
-            print("result", first_nb * second_nb)
-        elif operator == "-":
-            print("result", first_nb - second_nb)
-        elif operator == "/":
-            if second_nb == 0:
-                print("can't devise by 0")
+        currency = input("(1)normal currency\n(2)Bitcoin\n-")
+        if currency == "1":
+            c = CurrencyRates()
+            nb_to_convert = float(input("Insert number to convert\n-"))
+            from_currency = input("Insert currency, Example: USD/EUR...\n-").upper()
+            to_currency = input("Insert currency to convert to, Example: USD/EUR...\n-").upper()
+            print(nb_to_convert, from_currency, " = ", c.convert(from_currency, to_currency, nb_to_convert),
+                  to_currency)
+        elif currency == "2":
+            while True:
+                conversion = input("(1)From BTC to a currency\n(2)From a currency to BTC\n")
+                if conversion == "1":
+                    b = BtcConverter()
+                    nb_to_convert = float(input("Insert number to convert\n-"))
+                    to_currency = input("Insert currency to convert to, Example: USD/EUR...\n-").upper()
+                    print(nb_to_convert, "BTC", " = ", b.convert_btc_to_cur(nb_to_convert, to_currency),
+                          to_currency)
+                elif conversion == "2":
+                    b = BtcConverter()
+                    nb_to_convert = float(input("Insert number to convert\n-"))
+                    from_currency = input("Insert currency to convert from, Example: USD/EUR...\n-").upper()
+                    print(nb_to_convert, from_currency, " = ", b.convert_to_btc(nb_to_convert, from_currency),
+                          "BTC")
+                else:
+                    print("Invalid option")
+                    continue
+                break
+        else:
+            print("Invalid option")
+        break
+
+
+while True:
+    mode = input("pick a mode:\n(1)calculator mode\n(2)conversion mode\n(3)Live currency conversion mode\n-")
+    # calculator mode
+    if mode == "1":
+        first_nb = float(input("first number"))
+        second_nb = float(input("second number"))
+
+        # if the user inputs something other than *, -, /, +
+        # the user will be asked to pick the operator again
+        while True:
+            operator = input("pick an operator: *, -, /, +")
+            if operator == "*":
+                print("result", first_nb * second_nb)
+            elif operator == "-":
+                print("result", first_nb - second_nb)
+            elif operator == "/":
+                if second_nb == 0:
+                    print("can't devise by 0")
+                else:
+                    print("result", first_nb / second_nb)
+            elif operator == "+":
+                print("result", first_nb + second_nb)
             else:
-                print("result", first_nb / second_nb)
-        elif operator == "+":
-            print("result", first_nb + second_nb)
-        else:
-            print("invalid option")
-            continue
-        break
-
-
-# convertion mode
-elif mode == "2":
-    while True:
-        category = input("Which category do you want:\n(1)Planar_angle\n(2)time\n-")
-        if category == "1":
-            angle_plan()
-        elif category == "2":
-            time()
-        else:
-            print("invalid option")
-            continue
-        break
+                print("invalid option")
+                continue
+            break
+    # convertion mode
+    elif mode == "2":
+        while True:
+            category = input("Which category do you want:\n(1)Planar_angle\n(2)time\n-")
+            if category == "1":
+                angle_plan()
+            elif category == "2":
+                time()
+            else:
+                print("invalid option")
+                continue
+            break
+    # Currency conversion mode
+    elif mode == "3":
+        currency_conversion()
+    else:
+        print("Invalid option")
+        continue
+    break
 print("bye!")
